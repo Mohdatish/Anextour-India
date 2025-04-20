4c2b7dbd383bedde989c5696788b24be5cceed22
Mohd Atish <atishmohd7786@gmail.com>
2025-04-20 11:54:41 +0530

    changes

--- src/comp/card/Completed.tsx
+++ src/comp/card/Completed.tsx
@@ -3,33 +3,36 @@ import "../../../src/app/styles/bookings.css"
 
 const Completed = () => {
     return (
-        <div className='upcoming'>
-            <div className="booking-card">
-                <div className="booking-image">
-                    <img src="/assets/image.jpg" alt="Avataara Resort & Spa" />
-                </div>
-                <div className="booking-details">
-                    <div className='d-flex gap-4'>
-                        <div className="hotel-name">
-                            Avataara Resort & Spa
-                            <div className="location">Mohali, India</div>
+        <div className='completed'>
+            <div className="container-compl">
+                <div className="flight-info">
+                    <div className="flight-route">New Delhi → Dubai</div>
+                    <div className="flight-details">One Way Flight • Booking ID - 10000000</div>
+                    <div className="details-container">
+                        <div className="departure">
+                            <div className="label">FROM</div>
+                            <div className="time">Thu 31 Oct 2023 12:00 PM</div>
+                            <div className="airport">DEL - New Delhi</div>
+                            <div className="status">Completed</div>
                         </div>
-                        <div >
-                            <div className="rating">
-                                ★★★★★ 4.5 (143 ratings)
-                            </div>
-                        </div>
-                    </div>
-                    <div className='d-flex gap-5 justify-content-start align-items-center'>
-                        <div>
-                            <div className="booking-id">Thu 31 Oct 2023 - Fri 1 Nov 2023</div>
-                            <div className="confirmed">Completed</div>
+                        <div className="arrival">
+                            <div className="label">TO</div>
+                            <div className="time">Fri 1 Nov 2023 04:00 PM</div>
+                            <div className="airport">DXB - Dubai</div>
                         </div>
                     </div>
                 </div>
-                <div className='amount'>$2500</div>
+                <div className="airline-info">
+                    <div className="airline-logo">
+                        <img src="/assets/image.jpg" alt="IndiGo" style={{ width: "100px", height: "100px", objectFit: "cover" }} />
+                    </div>
+                    <div className="airline-details">
+                        <div>IndiGo</div>
+                        <div>6E909 Economy</div>
+                    </div>
                 </div>
-
+                <button className="manage-booking-button">View & Manage Booking</button>
+            </div>
         </div>
 
     )
